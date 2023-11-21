87e840f4e8b445507322825df9c248dc4d57b8e9
osvaldo <osvaldo@flip.net.br>
2023-11-21 14:13:20 -0300

    change for support php versions lower than 8.0

--- src/DiscordLogger/LogHandler.php
+++ src/DiscordLogger/LogHandler.php
@@ -35,7 +35,7 @@ class LogHandler extends AbstractProcessingHandler
         $this->ignoreExceptions = $channelConfig['ignore_exceptions'] ?? false;
     }
 
-    public function write(array|LogRecord $record): void
+    public function write($record): void
     {
         if ($record instanceof LogRecord) {
             $record =  $record->toArray();
